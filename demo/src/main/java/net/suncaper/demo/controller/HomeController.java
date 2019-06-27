b58caabadbc55b83797f6e3d486a427f172dba19
scuzyy <hzbszyy@foxmail.com>
2019-06-27 20:41:31 +0800

    购物车

--- demo/src/main/java/net/suncaper/demo/controller/HomeController.java
+++ demo/src/main/java/net/suncaper/demo/controller/HomeController.java
@@ -1,5 +1,6 @@
 package net.suncaper.demo.controller;
 
+import net.suncaper.demo.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static net.suncaper.demo.controller.UserController.getCookieByName;
 
 
 @Controller
@@ -24,8 +28,14 @@ public class HomeController {
     }
 
     @GetMapping("/shopping-cart")
-    public String getShoppingCart(){
-        return "/pages/shopping-cart.html";
+    public String getShoppingCart(HttpServletRequest request){
+//        if(getCookieByName(request,"userMailAddress") != null) {
+//            String userMailaddress = getCookieByName(request,"userMailAddress").getValue();
+//            return "/pages/user/Personal-center.html";
+//        }
+//        else
+//            return "redirect:/home";
+        return "/pages/shoppingcart.html";
     }
 
 }
