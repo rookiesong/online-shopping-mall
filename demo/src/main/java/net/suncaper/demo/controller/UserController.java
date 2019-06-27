298b5c467dc609eb20d5d522a01ca739d8315c6b
rookiesong <32297925+rookiesong@users.noreply.github.com>
2019-06-27 17:23:29 +0800

    update

--- demo/src/main/java/net/suncaper/demo/controller/UserController.java
+++ demo/src/main/java/net/suncaper/demo/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
@@ -73,7 +72,6 @@ public class UserController {
             else {
                 model.addAttribute("status","密码错误");
                 return "2";
-
             }
         }
         else
--- demo/src/main/java/net/suncaper/demo/domain/Product.java
+++ demo/src/main/java/net/suncaper/demo/domain/Product.java
@@ -68,6 +68,7 @@ public class Product {
         this.producer = producer;
         this.producingArea = producingArea;
         this.stock = stock;
+
     }
 
     /**
