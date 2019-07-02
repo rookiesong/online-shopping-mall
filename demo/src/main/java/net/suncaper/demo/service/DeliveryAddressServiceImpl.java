633711580b3aea1d2f5c145f86017d474538cdcd
rookiesong <32297925+rookiesong@users.noreply.github.com>
2019-07-02 15:36:21 +0800

    update

--- demo/src/main/java/net/suncaper/demo/service/DeliveryAddressServiceImpl.java
+++ demo/src/main/java/net/suncaper/demo/service/DeliveryAddressServiceImpl.java
@@ -3,18 +3,21 @@ package net.suncaper.demo.service;
 import net.suncaper.demo.domain.DeliveryAddress;
 import net.suncaper.demo.domain.DeliveryAddressExample;
 import net.suncaper.demo.mapper.DeliveryAddressMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class DeliveryAddressServiceImpl implements DeliveryAddressService{
+    @Autowired
     private DeliveryAddressMapper deliveryAddressMapper;
     @Override
     public List<DeliveryAddress> findDeliveryAddress(String userMailAddress) {
         DeliveryAddressExample example = new DeliveryAddressExample();
-        example.createCriteria().andUserMailaddressEqualTo(userMailAddress);
-        return deliveryAddressMapper.selectByExample(example);
+        example.createCriteria().andUserMailaddressEqualTo("%"+userMailAddress+"%");
+        List<DeliveryAddress> deliveryAddresses =deliveryAddressMapper.selectByExample(example);
+        return deliveryAddresses;
     }
 
     @Override
